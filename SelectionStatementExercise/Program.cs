50f5913fb4282477999936edff5f5d2ef91346ac
Colton Briggs <wolflink013@gmail.com>
2025-06-30 08:13:39 -0700

    find the number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+        static Random r = new Random();
+        static int favNumber = r.Next(1, 1000);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("Guess my favorite number:");
+            Console.WriteLine("It is between 1 and 1000");
+            guess();
+
         }
+
+        static void guess()
+        {
+            int userInput = int.Parse(Console.ReadLine());
+            
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("You guessed my favorite number!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Your guess is too low!");
+                guess();
+            }else
+            {
+                Console.WriteLine("Your guess is too high!");
+                guess();
+            }
+            
+        } 
+        
     }
 }
